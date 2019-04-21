c46702435837699c4ae965c13655df39c5e21df7
kalyani <chennupatikalyani@gmail.com>
2019-04-20 20:23:27 -0400

    Reverted

--- Action.c
+++ Action.c
@@ -1,9 +1,4 @@
-	
-
-	Action()
-{
-	 // This is my first GIT Pull and Commit
-		web_custom_request("web_custom_request",
+	web_custom_request("web_custom_request",
 		"URL=www.example.com",
 		"Method=GET",
 		"TargetFrame=",
@@ -11,5 +6,8 @@
 		"Referer=",
 		"Body=",
 		LAST);
+
+	Action()
+{
 	return 0;
 }
